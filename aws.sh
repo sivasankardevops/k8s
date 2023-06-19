f15a6f72d2f57212014e1db5f5d241c651f22c5b
sivasankardevops <42856513+sivasankardevops@users.noreply.github.com>
2023-06-19 16:05:55 +0530

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-aws configure set aws_access_key_id "" --profile siva && aws configure set aws_secret_access_key "" --profile siva && aws configure set region "us-west-2" --profile siva && aws configure set output "json" --profile siva
+mvn clean package
 aws s3api put-object --bucket azure-devops-sprint-boot-output --key /home/vsts/work/1/s/target/spring-boot-web.jar
