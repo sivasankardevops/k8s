0ff21a2e7f7801be6fb730c009375f8fcaa9b852
sivasankardevops <42856513+sivasankardevops@users.noreply.github.com>
2023-06-19 16:19:59 +0530

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mvn clean package
-aws s3api put-object --bucket azure-devops-sprint-boot-output --key /home/ubuntu/vsts/work/1/s/target/spring-boot-web.jar
+aws s3api put-object --bucket azure-devops-sprint-boot-output --key /home/ubuntu/1/s/target/spring-boot-web.jar
